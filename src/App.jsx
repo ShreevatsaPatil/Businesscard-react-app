f9a55cc8814328365921c3041a7cd8620474bc67
ShreeVatsa <01fe20bcs314@kletech.ac.in>
2023-03-19 15:26:57 +0530

    mjnf

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ function App() {
               <img src={userAvatar} className="avatar" alt="React logo" />
             </div>
             <div>
-              <h2 className="name">Priti Solanki</h2>
-              <h3 className="designation">Solution Architect</h3>
-              <h4 className="website"><a href="https://pritisolanki.com/" target="_blank">pritisolanki.com</a></h4>
+              <h2 className="name">Shreevatsa Patil</h2>
+              <h3 className="designation">Software Engineer</h3>
+              <h4 className="website"><a href="https://pritisolanki.com/" target="_blank">Dcc project</a></h4>
               <button><a href="mailto:norpely@donotspam.com"><img src={mail} className="emailIcon" /><span class="btntext">Email</span></a></button>
               <h2 class="remove-space">About</h2>
               <p>I am a solution architect with a particular interest in making things simple. I try to keep up with frontend technologies and best practices, and am always looking for new things to learn.</p>
